b629c92b2e5913175bb7871939814bd0803f361f
m.bukin <m.bukin@upstars.com>
2025-04-22 17:46:30 +0300

    dsadad

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,114 +1,55 @@
 import 'dart:async';
 
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:casino_da_povoa/cubit/places_cubit.dart';
 import 'package:casino_da_povoa/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  double progress = 0;
-
   @override
   void initState() {
     super.initState();
-    _startLoading();
-    context.read<GuideCubit>().loadGuides();
-  }
-
-  void _startLoading() {
-    Timer.periodic(const Duration(milliseconds: 50), (timer) {
-      if (progress >= 1) {
-        timer.cancel();
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const HomePage()),
-        );
-      } else {
-        setState(() {
-          progress += 0.02;
-        });
-      }
+    initialization();
+    Future.delayed(const Duration(seconds: 2), () async {
+      FlutterNativeSplash.remove();
+      Future.delayed(const Duration(seconds: 2), () async {
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        }
+      });
     });
   }
 
+
+  void initialization() async {
+    final TrackingStatus status =
+    await AppTrackingTransparency.trackingAuthorizationStatus;
+    if (status == TrackingStatus.notDetermined) {
+      await AppTrackingTransparency.requestTrackingAuthorization();
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Image.asset(
-            'assets/images/splash_background.png',
-            fit: BoxFit.cover,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100.0),
-              Center(
-                child: Container(
-                  width: screenWidth * 0.9,
-                  padding: const EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Text(
-                    'Rides\nDa Povoa',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 58,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              const Spacer(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF1E1E1E),
-                    borderRadius: BorderRadius.circular(18),
-                    border: Border.all(color: const Color(0xFF1E1E1E), width: 18),
-                  ),
-                  padding: const EdgeInsets.all(4),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: LinearProgressIndicator(
-                          value: progress,
-                          minHeight: 20,
-                          backgroundColor: Colors.black,
-                          valueColor: const AlwaysStoppedAnimation<Color>(Colors.orange),
-                        ),
-                      ),
-                      Text(
-                        "${(progress * 100).toInt()}%",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-            ],
-          ),
+          Image.asset('assets/images/l2.png', fit: BoxFit.cover),
         ],
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import app_set_id
+import flutter_inappwebview_macos
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSetIdPlugin.register(with: registry.registrar(forPlugin: "AppSetIdPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
